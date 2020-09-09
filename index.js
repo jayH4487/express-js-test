6389deba9b53c9c90face07c41da50cb6bb5116a
jayH4487 <59086697+jayH4487@users.noreply.github.com>
2020-09-09 18:28:01 +0100

    post & put member

--- index.js
+++ index.js
@@ -2,29 +2,18 @@ const express = require("express")
 const path = require("path")
 
 const logger = require("./middleware/logger")
-const members = require("./Members")
-
 
 const app = express()
 
 
 app.use(logger)
 
-app.get("/api/members", (req, res) => res.json(members))
-
-app.get("/api/members/:id", (req, res) => {
-
-    const member = members.filter(member => member.id === Number(req.params.id))
-
-    if (member.length !== 0) {
-        res.json(member)
-    } else {
-        res.status(400).json({ msg: `No member with the id of ${req.params.id}` })
-    }
-})
-
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
 app.use(express.static(path.join(__dirname, "public")))
+
+app.use("/api/members", require("./routes/api/members"))
 
 const PORT = process.env.PORT || 5000
 
---
+++ routes/api/members.js
@@ -0,0 +1,59 @@
+const express = require("express")
+const uuid = require("uuid")
+
+let members = require("../../Members")
+
+const router = express.Router()
+
+router.get("/", (req, res) => res.json(members))
+
+router.get("/:id", (req, res) => {
+
+    const member = members.filter(member => member.id === Number(req.params.id))
+
+    if (member.length !== 0) {
+        res.json(member)
+    } else {
+        res.status(400).json({ msg: `No member with the id of ${req.params.id}` })
+    }
+})
+
+router.post("/", (req, res) => {
+    const newMember = {
+        id: uuid.v4(),
+        name: req.body.name,
+        email: req.body.email,
+        status: "active"
+    }
+
+    if (newMember.name === undefined || newMember.email === undefined) {
+        res.status(400).json({ msg: "Please include a name and email" })
+    } else {
+        members = [...members, newMember]
+        res.send(req.body)
+    }
+})
+
+router.put("/:id", (req, res) => {
+
+    const [memberToUpdate] = members.filter(member => member.id === Number(req.params.id))
+
+    if (memberToUpdate === undefined) {
+        res.status(400).json({ msg: `No member with the id of ${req.params.id}` })
+    } else {
+        const updatedMember = {
+            ...memberToUpdate,
+            name: req.body.name || memberToUpdate.name,
+            email: req.body.email || memberToUpdate.email
+        }
+
+        members = members.map(member => {
+            return member.id === Number(req.params.id)
+                ? updatedMember
+                : member
+        })
+        res.send(members)
+    }
+})
+
+module.exports = router
