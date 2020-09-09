04d7e52856906e7b7c419ed96a35391dae28f7e8
jayH4487 <59086697+jayH4487@users.noreply.github.com>
2020-09-09 17:17:50 +0100

    get member

--- index.js
+++ index.js
@@ -12,6 +12,18 @@ app.use(logger)
 
 app.get("/api/members", (req, res) => res.json(members))
 
+app.get("/api/members/:id", (req, res) => {
+
+    const member = members.filter(member => member.id === Number(req.params.id))
+
+    if (member.length !== 0) {
+        res.json(member)
+    } else {
+        res.status(400).json({ msg: `No member with the id of ${req.params.id}` })
+    }
+})
+
+
 app.use(express.static(path.join(__dirname, "public")))
 
 const PORT = process.env.PORT || 5000
